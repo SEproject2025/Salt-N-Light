cf749eb6d6afd23ee68da54cf95e26ced8804c44
Jez Abot <jez.abot@students.pcci.edu>
2024-10-28 22:54:19 -0500

    Successfully integrated PostgreSQL

--- Backend/saltnlight/settings.py
+++ Backend/saltnlight/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'saltnlight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'saltnlight',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
