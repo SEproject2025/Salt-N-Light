d39b1026de30d19b22e9e541e785d6a09d0f0a0f
Andrew Teesdale, Jr <andrew+git@f40c40.com>
2024-11-02 22:32:35 -0500

    Update Requirements and Make Postgres Optional for Dev Made only use PostgreSQL when USE_POSTGRES environment variable is set. Added changes to requirements.

--- Backend/saltnlight/settings.py
+++ Backend/saltnlight/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,8 +94,23 @@ DATABASES = {
         'PASSWORD': 'saltnlight',
         'HOST': 'localhost',
         'PORT': '5432',
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'saltnlight',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+    'sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
+
+if os.getenv('USE_POSTGRES'):
+    DATABASES['default'] = DATABASES['postgres']
 
 
 # Password validation
