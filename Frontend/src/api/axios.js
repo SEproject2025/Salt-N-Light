6eda73c89bc00d57759471e5cc687d7e02be329a
caleblind <57119431+caleblind@users.noreply.github.com>
2025-03-19 10:41:06 -0400

    Update axios config

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set the base URL for all API requests
 const api = axios.create({
-  baseURL: "https://www.baptist.coffee/", // Add your root API URL
+  baseURL: "https://baptist.coffee/", // Add your root API URL
   timeout: 10000, // Optional: Set a timeout (in ms)
   headers: {
     "Content-Type": "application/json",
