59f7976b5dfe33807717922f0dc53a0f850ee169
caleblind <57119431+caleblind@users.noreply.github.com>
2025-04-23 08:17:00 -0400

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Set the base URL for all API requests
 const api = axios.create({
-  baseURL: "https://evangelium.app",
+  baseURL: "https://api.evangelium.app",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
