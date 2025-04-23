e75c72aa96fd89e9a39ee31be47c812f4e1005a8
caleblind <57119431+caleblind@users.noreply.github.com>
2025-04-23 07:21:15 -0400

    Fixing npm dependencies and config to work on hosted server

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Set the base URL for all API requests
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://evangelium.app",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
